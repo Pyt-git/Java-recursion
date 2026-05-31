ddc46b17eb4e19ffdad20ae6aa35157ea5151882
Pyt-git <weicky0804@gmail.com>
2026-05-31 14:05:29 -0400

    Update test-ContainsValue.java

--- tests/test-ContainsValue.java
+++ tests/test-ContainsValue.java
@@ -5,5 +5,35 @@ class ContainsValueTest {
 
   void testValueNotFound() {
     int [] arr = {2, 3, 4, 5, 9};
-    assertFalse(
+    assertFalse(ContainsValue.contains(arr, 7)); 
+  }
+
+  void testValueFound() {
+    int [] arr = {-3, -4, -6, 0, 9};
+    assertTrue(ContainsValue.contains(arr, -3));
+  }
+
+  void testValueNotFoundEmptyArray() {
+    int [] arr = {};
+    assertFalse(ContainsValue.contains(arr, 8)); 
+  }
+
+  void testValueFoundSingleElementArray() {
+    int [] arr = {0}; 
+    assertTrue(ContainsValue.contains(arr, 0));
+  }
+
+  void testValueNotFoundSingleElementArray() {
+    int [] arr = {3};
+    assertFalse(ContainsValue.contains(arr, 49));
+  }
+
+  void testMultipleValues() {
+    int [] arr = {2, 0, -7, 4, 9, 13, 5};
+    assertFalse(ContainsValue.contains(arr, 73));
+    assertTrue(ContainsValue.contains(arr, 4)); 
+    assertFalse(ContainsValue.contains(arr, 97)); 
+    assertTrue(ContainsValue.contains(arr, 0));
+  }
+}
     
