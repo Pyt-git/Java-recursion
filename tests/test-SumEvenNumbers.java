99e3623e53005d21149476fb02264f7c18ca875e
Pyt-git <weicky0804@gmail.com>
2026-05-31 14:28:31 -0400

    Update test-SumEvenNumbers.java

--- tests/test-SumEvenNumbers.java
+++ tests/test-SumEvenNumbers.java
@@ -6,4 +6,37 @@ class SumEvenNumbersTest {
   @Test 
   void testSingleEvenElementSum() {
     int [] arr = {4};
+    assertEquals(4, SumEvenNumbers.sumEvenArray(arr)); 
+  } 
+
+  @Test
+  void testSingleOddElementSum() {
+    int [] arr = {9}; 
+    assertEquals(0, SumEvenNumbers.sumEvenArray(arr));
+  }
+
+  @Test
+  void testSumOfEvenPositiveElements() {
+    int [] arr = {2, 3, 4, 7, 9}; 
+    assertEquals(6, SumEvenNumbers.sumEvenArray(arr)); 
+  } 
+  
+  @Test
+  void testSumOfEvenNegativeElements() { 
+    int [] arr = {-4, -2, -3, -8, -6};
+    assertEquals(-20, SumEvenNumbers.sumEvenArray(arr));
+  }
+
+  @Test
+  void testMixedSignValues() {
+    int [] arr = {2, -4, 7, 8, -2}; 
+    assertEquals(4, SumEvenNumbers.sumEvenArray(arr)); 
+  }
+
+  @Test
+  void testEmptyArraySum() {
+    int [] arr = {};
+    assertEquals(0, SumEvenNumbers.sumEvenArray(arr)); 
+  } 
+}
       
