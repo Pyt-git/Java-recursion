388a35c04c979fa5b6aed2b444519219381ddf2b
Pyt-git <weicky0804@gmail.com>
2026-05-31 14:12:44 -0400

    Update test-SumEvenNumbers.java

--- tests/test-SumEvenNumbers.java
+++ tests/test-SumEvenNumbers.java
@@ -1,1 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class SumEvenNumbersTest {
+
+  @Test 
+  void testSingleEvenElementSum() {
+    int [] arr = {4};
+      
